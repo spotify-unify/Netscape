1be30221e4b007d6ca02292e2d60672351c0c80d
Karl Johan Krantz <schwomp@gmail.com>
2015-05-16 16:22:02 +0200

    Cleaned source repo

--- src/app/index.js
+++ src/app/index.js
@@ -1,11 +1,9 @@
 'use strict';
 
 import MainCtrl from './main/main.controller';
-import NavbarCtrl from '../app/components/navbar/navbar.controller';
 
 angular.module('methuselah', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'mgcrea.ngStrap'])
   .controller('MainCtrl', MainCtrl)
-  .controller('NavbarCtrl', NavbarCtrl)
 
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
