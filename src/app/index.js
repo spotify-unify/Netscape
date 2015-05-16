950655a76a2653bb6225e93639fceeca48b1dfac
Karl Johan Krantz <schwomp@gmail.com>
2015-05-16 16:48:32 +0200

    Add base for second-page

--- src/app/index.js
+++ src/app/index.js
@@ -1,16 +1,22 @@
 'use strict';
 
 import MainCtrl from './main/main.controller';
+import SecondPageCtrl from './secondPage/secondPage.controller';
 
 angular.module('methuselah', ['ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngResource', 'ui.router', 'mgcrea.ngStrap'])
   .controller('MainCtrl', MainCtrl)
-
+  .controller('SecondPageCtrl', SecondPageCtrl)
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
       .state('home', {
         url: '/',
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
+      })
+      .state('second-page', {
+        url: '/second-page',
+        templateUrl: 'app/secondPage/secondPage.html',
+        controller: 'SecondPageCtrl'
       });
 
     $urlRouterProvider.otherwise('/');
