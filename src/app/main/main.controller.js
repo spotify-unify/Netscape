c4449d8bd3cfd7b169f2131998ba394c660b60af
nordberg <nordberg@github.com>
2015-05-16 22:06:34 +0200

    Random background at refresh

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -2,6 +2,14 @@
 
 var MainCtrl = ["$scope", function($scope) {
 
+	var bgPath = "./assets/images/";
+	var bgs = ["background01.jpg", "background02.jpg", "background03.jpg", "background04.jpg",
+		"background05.jpg", "background06.jpg", "background07.jpg"];
+
+	var bgIdx = Math.floor(bgs.length * Math.random());
+
+	$('body').css('background-image', 'url(' + bgPath + bgs[bgIdx] +')');
+
     $scope.details = "";
 
   $scope.buttonClicked = function() {
