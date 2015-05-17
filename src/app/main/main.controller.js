1bc4cf49bd0059ba3c58bbe5b2a75fe3fd2d9da6
fika4life <fistewie@gmail.com>
2015-05-17 11:53:59 +0200

    adds tagline first page

--- src/app/main/main.controller.js
+++ src/app/main/main.controller.js
@@ -3,8 +3,8 @@
 var MainCtrl = ["$scope", "$state", function($scope, $state) {
 
 	var bgPath = "./assets/images/";
-	var bgs = ["background01.jpg", "background02.jpg", "background03.jpg", "background04.jpg",
-		"background05.jpg", "background06.jpg", "background07.jpg"];
+	var bgs = ["background01.jpg", "background02.jpg",  "background04.jpg",
+	 "background06.jpg", "background07.jpg"];
 
 	var bgIdx = Math.floor(bgs.length * Math.random());
 
